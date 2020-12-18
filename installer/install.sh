32c9501732fdecb127aaa4d8156d2f932fc4dd24
HyunsoooAhn <hsahn@smartworks.net>
2020-12-18 11:33:36 +0900

    https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/ 로 수정

--- installer/install.sh
+++ installer/install.sh
@@ -2,42 +2,42 @@ if [ -f "config.production.js" ] ; then
   echo "config.production.js exist"
 else
   echo "config.production.js create"
-  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/fmsim/installer/config.production.js
+  curl -O https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/config.production.js
 fi
 
 if [ -f "start.sh" ] ; then
   echo "start.sh exist"
 else
   echo "start.sh create"
-  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/fmsim/installer/start.sh
+  curl -O https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/start.sh
 fi
 
 if [ -f "stop.sh" ] ; then
   echo "stop.sh exist"
 else
   echo "stop.sh create"
-  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/fmsim/installer/stop.sh
+  curl -O https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/stop.sh
 fi
 
 if [ -f "upgrade.sh" ] ; then
   echo "upgrade.sh exist"
 else
   echo "upgrade.sh create"
-  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/fmsim/installer/upgrade.sh
+  curl -O https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/upgrade.sh
 fi
 
 if [ -f "migrate.sh" ] ; then
   echo "migrate.sh exist"
 else
   echo "migrate.sh create"
-  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/fmsim/installer/migrate.sh
+  curl -O https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/migrate.sh
 fi
 
 if [ -f "docker-compose.yml" ] ; then
   echo "docker-compose.yml exist"
 else
   echo "docker-compose.yml create"
-  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/fmsim/installer/docker-compose.yml
+  curl -O https://raw.githubusercontent.com/HyunsoooAhn/fmsim/main/installer/docker-compose.yml
 fi
 
 chmod u+x start.sh
